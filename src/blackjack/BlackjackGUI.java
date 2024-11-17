1a1d625477d74c78d287d35490ef6537bc49690a
David Díaz <143227166+Cuervy117@users.noreply.github.com>
2024-11-17 09:40:45 -0600

    boton blackjack dirige al juego, centré pantalla de bj enmedio

--- src/blackjack/BlackjackGUI.java
+++ src/blackjack/BlackjackGUI.java
@@ -34,6 +34,7 @@ public class BlackjackGUI extends JFrame {
     private void inicializarInterfaz() {
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
         JPanel panelCartas = new JPanel();
