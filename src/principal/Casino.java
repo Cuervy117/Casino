a4f12c5b44f0e80c85811dee0cbf3d82a93cb70f
David Díaz <143227166+Cuervy117@users.noreply.github.com>
2024-11-17 21:27:32 -0600

    lo que llevo pibes

--- src/principal/Casino.java
+++ src/principal/Casino.java
@@ -6,8 +6,11 @@ import ruleta.RuletaCasino;
 import tragamonedas.Tragamonedas;
 import usuario.Usuario;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -35,25 +38,40 @@ public class Casino extends JFrame {
         this.usuario = usuario;
         saldo.setText("Saldo : " + usuario.getCartera().getSaldo());
         nombre.setText("Nombre : " + usuario.getNombre());
+
         blackJackButton.addActionListener(e -> {
             BlackjackGUI bj = new BlackjackGUI(usuario);
             historial.add(usuario.crearMemento());
+            bj.addWindowListener(new WindowAdapter(){
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    saldo.setText("Saldo : " + usuario.getCartera().getSaldo()); // Actualizar saldo
+                }
+            });
             bj.setVisible(true);
-        }); 
-        //Agregando funcionalidad de ruleta
-        ruletaButton.addActionListener(e ->{ 
-            RuletaCasino rc = new RuletaCasino(null);
         });
 
         ruletaButton.addActionListener(e -> {
-            RuletaCasino ruletaCasino = new RuletaCasino();
+            RuletaCasino ruletaCasino = new RuletaCasino(usuario);
             historial.add(usuario.crearMemento());
+            ruletaCasino.addWindowListener(new WindowAdapter(){
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    saldo.setText("Saldo : " + usuario.getCartera().getSaldo()); // Actualizar saldo
+                }
+            });
             ruletaCasino.setVisible(true);
         } );
 
         tragamonedasButton.addActionListener(e -> {
             Tragamonedas tragamonedas = new Tragamonedas(usuario);
             historial.add(usuario.crearMemento());
+            tragamonedas.addWindowListener(new WindowAdapter(){
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    saldo.setText("Saldo : " + usuario.getCartera().getSaldo()); // Actualizar saldo
+                }
+            });
             tragamonedas.setVisible(true);
         });
         retirarDineroButton.addActionListener(e -> {
@@ -67,17 +85,6 @@ public class Casino extends JFrame {
             }
         });
 
-        ruletaButton.addActionListener(e -> {
-            RuletaCasino ruletaCasino = new RuletaCasino();
-            historial.add(usuario.crearMemento());
-            ruletaCasino.setVisible(true);
-        } );
-
-        tragamonedasButton.addActionListener(e -> {
-            Tragamonedas tragamonedas = new Tragamonedas(usuario);
-            historial.add(usuario.crearMemento());
-            tragamonedas.setVisible(true);
-        });
         retirarDineroButton.addActionListener(e -> {
             String retirar = JOptionPane.showInputDialog(this, "Ingresa la cantidad que deseas retirar",
                     "Retiro de dinero", JOptionPane.PLAIN_MESSAGE);
@@ -89,11 +96,12 @@ public class Casino extends JFrame {
             }
         });
         desactivarCuentaButton.addActionListener(e -> {
-
             if(JOptionPane.showConfirmDialog(this, "¿Seguro que deseas desactivar tu cuenta?") == 1){
                 usuario.desactivarCuenta();
-                JFrame.EXIT_ON_CLOSE;
             }
+        });
+        cambiarDeCuentaButton.addActionListener(e -> {
+            System.
         });
     }
 
